519d3d6efa29c2355f8cd2eeeff4b9aca03e5d69
Leonardo de Oliveira da Costa <leoncosta@hotmail.com>
2023-10-23 21:30:51 -0300

    Atualização 2 para criar biblioteca

--- Atividade5/vite.config.ts
+++ Atividade5/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: 'src/index.ts', 
+      name: 'Atividade5Library', 
+      fileName: (format) => `atividade5-library.${format}.js`
+    },
+    rollupOptions: {
+      
+      external: ['react', 'react-dom'],
+      output: {
+        
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+});
+
